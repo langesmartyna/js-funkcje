1597f78a45976aeb298d236198be3e435261a6ab
langesmartyna <langesmartyna@gmail.com>
2021-01-21 14:32:23 +0100

    pierwszy zapis

---
+++ main.js
@@ -0,0 +1,12 @@
+firstNumber = (8);
+secondNumber =(10);
+
+function suma(firstNumber, secondNumber) {
+
+    let result = firstNumber + secondNumber;
+    return result;
+
+}
+
+console.log( suma(firstNumber, secondNumber) );
+
